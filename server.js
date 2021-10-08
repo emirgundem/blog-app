30f07352f41ab1cdda042728160686dd7b0d10b7
emirgundem <emirgndem@gmail.com>
2021-10-08 15:15:01 +0300

    bug fixed

--- server.js
+++ server.js
@@ -4,18 +4,14 @@ const port = 3000;
 const mongoose = require('mongoose');
 require('dotenv').config();
 const app = express();
-
-
-app.get('/homepage',(request,response)=>{
-    
-})
-
-
+app.use(express.json());
+//Routes
+const authRoute = require('./api/models/routes/auth');
+app.use('/api/auth',authRoute);
 
 //MongoDb Connection
 mongoose.connect(process.env.MONGO_CONNECTION,{useNewUrlParser:true,useUnifiedTopology:true},()=>{
      console.log('Connection Success');
-     
 })
 
 
